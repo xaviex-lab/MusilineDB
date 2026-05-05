3bcc01a2cfa2237d95dc55142df8dc2416bd6591
xaviex-lab <xaviex240@gmail.com>
2026-05-05 19:50:15 -0300

    adicionei STATIC_ROOT

--- core/settings.py
+++ core/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # adiciona essa linha!
 
 # Variáveis do Aplicativo
 APP_NAME = 'MusiLineDB'
